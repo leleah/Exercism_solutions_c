a9a35648834e5ce10e4c65c94f43db0c98b25755
leleah <alya.leah.13@gmail.com>
2025-02-24 20:56:23 +0300

    download

--- resistor-color/resistor_color.c
+++ resistor-color/resistor_color.c
@@ -1,18 +1,16 @@
 #include "resistor_color.h"
-#ifndef RESISTOR_COLOR_H
-#define RESISTOR_COLOR_H
-resistor_band_t expected[] = {BLACK,BROWN,RED,ORANGE,YELLOW,
-GREEN,BLUE,VIOLET,GREY,WHITE};
 
-uint16_t color_code(resistor_band_t color){
-    return color;
-}
-resistor_band_t *colors() {
-    for(int i=0;i<10;i++)
-    {
-        return color_code(expected[i]);
-    }
+resistor_band_t res;
+   resistor_band_t expected[] = { BLACK, BROWN, RED,    ORANGE, YELLOW,
+                                        GREEN, BLUE,  VIOLET, GREY,   WHITE };
+resistor_band_t color_code(resistor_band_t resistor){
+
+return expected[resistor];
     
-}  
-#endif
+}
+ resistor_band_t* colors(void){
+
+    return (&expected[0]);
+}
+
 
--- resistor-color/test-framework/resistor_color.h
+++
@@ -1,8 +0,0 @@
-#ifndef RESISTOR_COLOR_H
-#define RESISTOR_COLOR_H
-
-typedef enum {
-
-} resistor_band_t;
-
-#endif
