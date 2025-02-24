0b4169f5267cd0f93e913456b3210a7b25aa32f4
leleah <alya.leah.13@gmail.com>
2025-02-24 18:00:42 +0300

    download c resistor-color

--- resistor-color/resistor_color.c
+++ resistor-color/resistor_color.c
@@ -1,1 +1,18 @@
 #include "resistor_color.h"
+#ifndef RESISTOR_COLOR_H
+#define RESISTOR_COLOR_H
+resistor_band_t expected[] = {BLACK,BROWN,RED,ORANGE,YELLOW,
+GREEN,BLUE,VIOLET,GREY,WHITE};
+
+uint16_t color_code(resistor_band_t color){
+    return color;
+}
+resistor_band_t *colors() {
+    for(int i=0;i<10;i++)
+    {
+        return color_code(expected[i]);
+    }
+    
+}  
+#endif
+
