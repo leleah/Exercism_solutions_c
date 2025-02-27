1b4de38c255434982e699f8a5421ff69e9ac43d2
leleah <alya.leah.13@gmail.com>
2025-02-27 11:48:47 +0300

    download c queen-attack

--- queen-attack/queen_attack.c
+++ queen-attack/queen_attack.c
@@ -1,1 +1,32 @@
 #include "queen_attack.h"
+int board[8][8] = {
+    {2,4,3,5,6,3,4,2},
+    {1,1,1,1,1,1,1,1},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {1,1,1,1,1,1,1,1},
+    {2,4,3,5,6,3,4,2}
+};
+int blackWhite[8][8] = {
+    {2,2,2,2,2,2,2,2},
+    {2,2,2,2,2,2,2,2},
+    {2,2,2,2,2,2,2,2},
+    {1,1,1,2,2,2,2,2},
+    {2,2,2,2,2,2,2,2},
+    {2,2,2,2,2,2,2,2},
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1}
+};
+int piece = board[][];
+int color = blackWhite[][];
+attack_status_t can_attack(position_t queen_1, position_t queen_2)
+{
+    
+   if(x > 7 || x < 0 || y > 7 || y < 0)
+        return INVALID_POSITION;
+    if(piece == 0){
+        return INVALID_POSITION;  
+    }
+}
