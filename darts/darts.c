92fe6f33238905e9100f68195f027409a4d5d3a5
leleah <alya.leah.13@gmail.com>
2025-03-04 09:50:03 +0300

    download c darts

--- darts/darts.c
+++ darts/darts.c
@@ -3,23 +3,20 @@
 uint8_t score (coordinate_t landing_position)
 {
     uint8_t score=0;
-    uint8_t circle_radius = sqrt((landing_position.x*landing_position.x)+(landing_position.y*landing_position.y));  
-    if(circle_radius<=1)
+    float circle_radius = sqrt((landing_position.x*landing_position.x)+(landing_position.y*landing_position.y));  
+    if(circle_radius <= 1)
     {
         score = 10;
     }
-    if((circle_radius>1) && (circle_radius<=5))
+    else if(circle_radius<=5) 
     {
         score = 5;
     }
-    if((circle_radius>5) && (circle_radius<=10))
+   else  if(circle_radius<=10) 
     {
         score=1;
-		
-		
-		
     }
-    if(circle_radius>10)
+    else
     {
         score=0;
     }
