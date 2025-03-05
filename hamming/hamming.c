54ab7a8c6b14eb84086416aa6db4787296b8a656
leleah <alya.leah.13@gmail.com>
2025-03-05 17:18:24 +0300

    download c hammihg

--- hamming/hamming.c
+++ hamming/hamming.c
@@ -1,1 +1,23 @@
 #include "hamming.h"
+int compute(const char *lhs, const char *rhs)
+{
+    int flag=0;
+    while (*lhs != 0 && *rhs!=0){
+    if(*lhs != *rhs) 
+    {
+        flag++;
+    }
+        lhs++;
+        rhs++;
+    }
+  /*  while(*lhs && (*lhs == *rhs))
+    {
+        lhs++;
+        rhs++;
+    }
+    if(*lhs != *rhs) 
+    {
+        flag++;
+    }*/
+    return flag;
+}
