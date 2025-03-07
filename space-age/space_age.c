05a58a28106bb38c2666d602df83bede0963e382
leleah <alya.leah.13@gmail.com>
2025-03-07 13:46:41 +0300

    download c space-age

--- space-age/space_age.c
+++ space-age/space_age.c
@@ -1,1 +1,7 @@
 #include "space_age.h"
+float orbital_period[]={0.2408467,0.61519726,1.0,1.8808158,11.862615,29.447498,84.016846,164.79132};
+float age(planet_t planet, int64_t seconds)
+{
+    //float ages = seconds*orbital_period[planet];
+    return (float)seconds/orbital_period[planet];
+}
