0e04556d6bfc73aa4156ea024827bf4f2c4d551c
leleah <alya.leah.13@gmail.com>
2025-03-08 19:48:55 +0300

    download c rna-transcribtion

--- rna-transcription/rna_transcription.c
+++ rna-transcription/rna_transcription.c
@@ -1,1 +1,15 @@
 #include "rna_transcription.h"
+char *to_rna(const char *dna)
+{
+    char *rna='0';
+    switch (*dna)
+    {
+        case 'G': (*rna++)='C';break;
+        case 'C': (*rna++)='G';break;
+        case 'T': (*rna++)='A';break;
+        case 'A': (*rna++)='U';break;
+            
+    }
+    return rna;
+    
+}
