9d980cc249083626a1f5ebbc0e04bdf0331c8988
leleah <alya.leah.13@gmail.com>
2025-03-12 10:07:48 +0300

    download c binary

--- binary/binary.c
+++ binary/binary.c
@@ -1,7 +1,7 @@
 #include "binary.h"
- int res=0;
+
 int convert(const char *input)
-{      
+{       int res=0;
         for(int j=((int)strlen(input)-1); j>=0; j-- )
         {
             if (input[j]!='0' && input[j]!= '1')
@@ -10,7 +10,8 @@ int convert(const char *input)
           }
             if(input[j]=='1'){
 
-             res+=pow(2,j);   
+             //res+=pow(2,j);   
+            res += (1 << (strlen(input) - 1 - j)); 
             }  
         }
         
