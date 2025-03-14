1bee12f0457a1ea84f96b68beabe72cf36ff21ca
leleah <alya.leah.13@gmail.com>
2025-03-14 17:05:04 +0300

    download c two-fer

--- two-fer/two_fer.c
+++ two-fer/two_fer.c
@@ -1,1 +1,11 @@
 #include "two_fer.h"
+
+void two_fer(char *buffer, const char *name)
+{char *first = "One for ";
+ char *second = ", one for me.";
+    if (name == NULL)
+    {
+        name= "you";
+    }
+    sprintf(buffer, "%s%s%s", first, name, second);
+}
