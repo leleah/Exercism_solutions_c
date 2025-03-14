ab37318c57308214cde5ab2c2190e9ad9ad93b4c
leleah <alya.leah.13@gmail.com>
2025-03-14 18:00:50 +0300

    download c raindrops

--- raindrops/raindrops.c
+++ raindrops/raindrops.c
@@ -1,1 +1,18 @@
 #include "raindrops.h"
+void convert(char result[], int drops)
+{
+    if (drops % 3 == 0)
+    {
+        sprintf(result,"Pling");
+    }
+    if (drops % 5 ==0)
+    {
+        sprintf(result,"Plang");
+    }
+    if (drops % 7 ==0)
+    {
+        sprintf(result,"Plong");
+    }
+    else if (drops %3 != 0 && drops %5 != 0 && drops % 7!=0)
+    {sprintf(result,"%c",drops);}
+}
--- raindrops/raindrops.h
+++ raindrops/raindrops.h
@@ -1,6 +1,6 @@
 #ifndef RAINDROPS_H
 #define RAINDROPS_H
-
+#include <stdio.h>
 void convert(char result[], int drops);
 
 #endif
