8523cf5fc6237bb0d1d105a03175645ed169e62f
leleah <alya.leah.13@gmail.com>
2025-03-24 19:12:00 +0300

    download c dnd-character

--- dnd-character/dnd_character.c
+++ dnd-character/dnd_character.c
@@ -1,19 +1,21 @@
 #include "dnd_character.h"
+
+dnd_character_t ret;
 int ability(void)
 {
-    int sum=0;
+    int sum=-1;
     srand(time(NULL));
-    for(int i =0;i <=6 ; i++)
-    {
-        for(int j =1;j<=6;j++)
-        {
+    
+        
             int a = (rand() % 6) + 1;  
             int b = (rand() % 6) + 1;  
             int c = (rand() % 6) + 1;  
             int d = (rand() % 6) + 1; 
             if ((a>0) && (b>0) && (c>0) && (d>0))
             {
-                if((a>b) && (c>b) && (d> b))
+                char arr[4];
+                arr[0]=a;arr[1]=b;arr[2]=c;arr[3]=d;
+                /*if((a>b) && (c>b) && (d> b))
                 {
                     sum = a+c+d;
                 }
@@ -28,21 +30,47 @@ int ability(void)
                 else if ((a>d)&&(b>d)&&(c>d))
                 {
                     sum = a+b+c;
+                }*/
+                int min=6,ind=0;
+                for(int i=0;i<4;i++)
+                {
+                   if(arr[i]<min){
+                       min=arr[i];
+                       ind=i;
+                   } 
                 }
+
+                for(int i=0;i<4;i++)
+                {
+                    if(i!=ind)
+                    {
+                        sum+=arr[i];
+                    }
+                }
+
+
+                
                
-            } 
-        }
+             
+        
     }
    return sum;
 }
 int modifier(int score)
 {
-    score=0;
+    score=(score/2)-5;
     return score;
 }
-dnd_character_t ret;
+
 dnd_character_t make_dnd_character(void)
 {
+    ret.strength=ability();
+    ret.dexterity=ability();
+    ret.constitution=ability();
+    ret.intelligence=ability();
+    ret.wisdom=ability();
+    ret.charisma=ability();
+    ret.hitpoints=10+modifier(ret.constitution);
     
     return ret;
 }
